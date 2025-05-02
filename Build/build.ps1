96f444061ac018282a63aad4bd44fff6681e9e4e
xuzhuoxi <mailxuzhuoxi@163.com>
2025-05-02 10:35:46 +0800

    en: 1. Add the CopyLocalLockFileAssemblies property in Infra-CSharp.csproj to automatically copy dependent projects to the build directory during the build process. 2. Add the publish script.
    cn:
    1. 在Infra-CSharp.csproj的中增加 CopyLocalLockFileAssemblies属性，在Build的过程中自动复制依赖项目到构造目录。
    2. 增加 publish脚本。

--- Build/build.ps1
+++ Build/build.ps1
@@ -8,3 +8,10 @@ Write-Host "Building solution with configuration: $configuration"
 Start-Process -NoNewWindow -Wait -FilePath "msbuild.exe" -ArgumentList "$solutionFile /p:Configuration=$configuration"
 
 Write-Host "Build completed!"
+
+# 如果用户传入 -publish 选项，则执行发布
+if ($publish) {
+    Write-Host "Publishing solution..."
+    Start-Process -NoNewWindow -Wait -FilePath "dotnet" -ArgumentList "publish $solutionFile --configuration $configuration --output ./publish"
+    Write-Host "Publish completed!"
+}
---
+++ Build/publish-release.bat
@@ -0,0 +1,10 @@
+@echo off
+set CONFIGURATION=Release
+set SOLUTION=../Infra-CSharp.sln
+
+echo Publishing solution with configuration: %CONFIGURATION%
+@REM msbuild %SOLUTION% /p:Configuration=%CONFIGURATION%
+dotnet publish %SOLUTION% /p:Configuration=%CONFIGURATION%
+
+echo publish completed!
+pause
